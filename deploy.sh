020ca4e8dd634503ee467bbe10f01966edca113b
ibroid <maulana858@gmail.com>
2025-07-23 19:20:18 +0700

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Nama image dan container
-IMAGE_NAME=laravel-franken
-CONTAINER_NAME=laravel-app
+IMAGE_NAME=daftar-online-larafran
+CONTAINER_NAME=daftar-online
 
 # Port container di host
 HOST_PORT=8080
@@ -16,17 +16,19 @@ docker run -d --name $CONTAINER_NAME -p $HOST_PORT:80 $IMAGE_NAME
 echo "📁 3. Menyalin .env.production ke container..."
 docker cp .env.production $CONTAINER_NAME:/var/www/html/.env
 
-echo "🔑 4. Generate Key..."
-docker exec -it $CONTAINER_NAME php artisan key:generate
+echo "⏳ 4. Menunggu Laravel siap..."
+sleep 5  # Beri waktu container startup
 
 echo "🔃 5. Menjalankan migrate dan seed..."
-docker exec -it $CONTAINER_NAME php artisan migrate --force
-docker exec -it $CONTAINER_NAME php artisan db:seed --force
+docker exec -it $CONTAINER_NAME php artisan migrate
+docker exec -it $CONTAINER_NAME php artisan db:seed
 
-echo "🧹 6. Membersihkan dan mencache konfigurasi..."
+echo "🔑 6. Generating key..."
+docker exec -t $CONTAINER_NAME php artisan key:generate
+
+echo "🧹 7. Membersihkan dan mencache konfigurasi..."
+docker exec -it $CONTAINER_NAME php artisan cache:clear
 docker exec -it $CONTAINER_NAME php artisan config:clear
 docker exec -it $CONTAINER_NAME php artisan config:cache
-docker exec -it $CONTAINER_NAME php artisan route:cache
-docker exec -it $CONTAINER_NAME php artisan view:cache
 
 echo "✅ Deploy selesai. Laravel berjalan di http://localhost:$HOST_PORT"
