8398cd16d524916ba45126c780967c6ea7b805e1
ibroid <maulana858@gmail.com>
2025-08-11 09:39:54 +0700

    update deploy.sh typo

--- deploy.sh
+++ deploy.sh
@@ -11,7 +11,7 @@ echo "🔧 1. Building Docker image..."
 docker build -t $IMAGE_NAME .
 
 echo "🚀 2. Menjalankan container..."
-docker run -d -v $(pwd)/storage:/app/storage \ --name $CONTAINER_NAME -p $HOST_PORT:80 $IMAGE_NAME
+docker run -d -v $(pwd)/storage:/app/storage --name $CONTAINER_NAME -p $HOST_PORT:80 $IMAGE_NAME
 
 echo "📁 3. Menyalin .env.production ke container..."
 docker cp .env.example $CONTAINER_NAME:/app/.env
